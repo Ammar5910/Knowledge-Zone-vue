e971696818c0ced58693c67c804defca6e95f08a
ammar5910 <ammar5910s@gmail.com>
2024-10-30 15:19:54 +0400

    up3
    upd 3 product array

---
+++ js/classes.js
@@ -0,0 +1,7 @@
+let classes= [
+    { name: 'Math', price: 100, seats: 5, location: 'Building A' },
+    { name: 'Science', price: 120, seats: 2, location: 'Building B' },
+    { name: 'English', price: 90, seats: 3, location: 'Building C' },
+    { name: 'History', price: 110, seats: 0, location: 'Building D' }, // Example out of stock
+    { name: 'Geography', price: 105, seats: 4, location: 'Building E' }
+]
