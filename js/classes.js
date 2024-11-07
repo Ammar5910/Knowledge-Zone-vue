a20a67a1ef0238dd6d162aa42269b6e09a0cd818
ammar5910 <ammar5910s@gmail.com>
2024-11-07 13:40:15 +0400

    upd 3
    Convert single product to products array
    
    Change data:product{} to data:products[]
    Initial array with 2-3 products

--- js/classes.js
+++ js/classes.js
@@ -1,7 +1,13 @@
 let classes= [
     { name: 'Math', price: 100, seats: 5, location: 'Building A' },
-    { name: 'Science', price: 120, seats: 2, location: 'Building B' },
-    { name: 'English', price: 90, seats: 3, location: 'Building C' },
-    { name: 'History', price: 110, seats: 0, location: 'Building D' }, // Example out of stock
-    { name: 'Geography', price: 105, seats: 4, location: 'Building E' }
+    { name: 'karate', price: 120, seats: 2, location: 'Building B' },
+    { name: 'Akido', price: 90, seats: 3, location: 'Building C' },
+    { name: 'Grappler', price: 110, seats: 5, location: 'Building D' }, // Example out of stock
+    { name: 'Geography', price: 105, seats: 4, location: 'Building E' },
+    { name: 'Python', price: 105, seats: 7, location: 'Building F' },
+    { name: 'judo', price: 105, seats: 4, location: 'Building G' },
+    { name: 'Kendo', price: 105, seats: 6, location: 'Building H' },
+    { name: 'Geography', price: 105, seats: 3, location: 'Building I' },
+    { name: 'Cooking', price: 105, seats: 4, location: 'Building G' }
+
 ]
