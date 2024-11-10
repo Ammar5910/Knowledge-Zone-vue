8ea74455a3481af47f56cadda860290eaceaf4a8
ammar5910 <ammar5910s@gmail.com>
2024-11-10 23:21:22 +0400

    Upd 5
    Sort by title implementation
    
    -Implement multi-attribute sorting
    
    Add location, price, seats sorting
    
    -Add ascending/descending toggle
    
    DESC/ASC functionality

--- javascript/classes.js
+++ javascript/classes.js
@@ -1,13 +1,12 @@
 let classes= [
     { name: 'Math', price: 100, seats: 5, location: 'Building A' },
-    { name: 'karate', price: 120, seats: 2, location: 'Building B' },
-    { name: 'Akido', price: 90, seats: 3, location: 'Building C' },
+    { name: 'Karate', price: 120, seats: 2, location: 'Building B' },
+    { name: 'Akido', price: 190, seats: 3, location: 'Building C' },
     { name: 'Grappler', price: 110, seats: 5, location: 'Building D' }, // Example out of stock
-    { name: 'Geography', price: 105, seats: 4, location: 'Building E' },
-    { name: 'Python', price: 105, seats: 7, location: 'Building F' },
-    { name: 'judo', price: 105, seats: 4, location: 'Building G' },
-    { name: 'Kendo', price: 105, seats: 6, location: 'Building H' },
-    { name: 'Geography', price: 105, seats: 3, location: 'Building I' },
-    { name: 'Cooking', price: 105, seats: 4, location: 'Building G' }
+    { name: 'Python', price: 90, seats: 7, location: 'Building F' },
+    { name: 'Judo', price: 125, seats: 4, location: 'Building G' },
+    { name: 'Kendo', price: 145, seats: 6, location: 'Building H' },
+    { name: 'Geography', price: 65, seats: 3, location: 'Building I' },
+    { name: 'Cooking', price: 85, seats: 4, location: 'Building G' }
 
 ]
