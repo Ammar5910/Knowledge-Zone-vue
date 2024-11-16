ca075628505d1cb12dd54fc0a6e977763e6c9ad7
ammar5910 <ammar5910s@gmail.com>
2024-11-16 15:51:38 +0400

    Upd 13
    modified some CSS
    updated the search function
    updated the class.js to have 10 classes with 5 seats of availability

--- javascript/classes.js
+++ javascript/classes.js
@@ -1,12 +1,12 @@
-let classes= [
-    { name: 'Math', price: 100, seats: 5, location: 'Building A' },
-    { name: 'Karate', price: 120, seats: 2, location: 'Building B' },
-    { name: 'Akido', price: 190, seats: 3, location: 'Building C' },
-    { name: 'Grappler', price: 110, seats: 5, location: 'Building D' }, // Example out of stock
-    { name: 'Python', price: 90, seats: 7, location: 'Building F' },
-    { name: 'Judo', price: 125, seats: 4, location: 'Building G' },
-    { name: 'Kendo', price: 145, seats: 6, location: 'Building H' },
-    { name: 'Geography', price: 65, seats: 3, location: 'Building I' },
-    { name: 'Cooking', price: 85, seats: 4, location: 'Building G' }
-
-]
+let classes = [
+  { name: 'Math', price: 100, seats: 5, location: 'UK' },
+  { name: 'Karate', price: 120, seats: 5, location: 'Tokyo' },
+  { name: 'Aikido', price: 190, seats: 5, location: 'Shanghai' },
+  { name: 'Grappler', price: 110, seats: 5, location: 'Kyoto' },
+  { name: 'Python Programming', price: 90, seats: 5, location: 'US' },
+  { name: 'Judo', price: 125, seats: 5, location: 'America' },
+  { name: 'Kendo', price: 145, seats: 5, location: 'Russia' },
+  { name: 'Geography', price: 65, seats: 5, location: 'Ohio' },
+  { name: 'Cooking Basics', price: 85, seats: 5, location: 'London' },
+  { name: 'Kung Fu', price: 185, seats: 5, location: 'China' },
+];
